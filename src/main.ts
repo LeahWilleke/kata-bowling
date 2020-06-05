5e38ae6b0032e6502c9e6a23511d9a2434a4536d
LeahWilleke <ellie.willeke@gmail.com>
2020-06-04 18:28:23 -0600

    Made notes for the next session.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 export function score(_rolls: number[]) {
 	//I want it to be defined properly. I currently want the outcome to be 0, like it's expected to be.
-	return 0;
+	return 0; //Make it so that it adds all the numbers up.
 }
 
 
 
 
 
-
+//Make a system that says X + X = A + X = A + X (etc), stop when you run out of numbers. A = Answer, X = A number.
 
 
 
