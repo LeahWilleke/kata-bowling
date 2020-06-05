d4335badd1db72a9dbd179aaecb8d82306ad93aa
LeahWilleke <ellie.willeke@gmail.com>
2020-06-04 18:17:51 -0600

    Purposely made an error. (;-_-',)

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,11 @@
 import { score } from '../src/main';
 
 describe('bowling', () => {
-  test('a gutter game should score 0', () => {
+  test('A gutter game should score 0.', () => {
     expect(score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(0);
   });
+  test('This should score 5.', () => {
+    expect(score([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 2, 0, 0, 0])).toBe(5);
+  });
 });
+
