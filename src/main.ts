50603ba7bc0a3cb734c8baff4b6316e0ad9971b2
LeahWilleke <ellie.willeke@gmail.com>
2020-06-04 18:06:43 -0600

    I fixed an error Arlo purposely made.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-export function score(_rolls: number[]) {}
+export function score(_rolls: number[]) {
+	//I want it to be defined properly. I currently want the outcome to be 0, like it's expected to be.
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
